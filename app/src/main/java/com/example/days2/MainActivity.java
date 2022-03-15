65a89c7800a367da63de8aaf0668ca06c3255de2
lutfialdrii <12050110415@students.uin-suska.ac.id>
2022-03-16 06:49:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/days2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.days2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_user);
+    }
+}
